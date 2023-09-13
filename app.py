4755cbe38f0031b875990af665b5d608745a0768
Xrlabsmia <120213306+Xrlabsmia@users.noreply.github.com>
2023-09-13 08:02:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,44 +8,111 @@ from gtts import gTTS
 from io import BytesIO
 import openai
 
-import streamlit as st
+openai.api_key = '{Your API Key}'
 
-st.title("ChatGPT-like ChatBot")
+if 'prompts' not in st.session_state:
+    st.session_state['prompts'] = [{"role": "system", "content": "You are a helpful assistant. Answer as concisely as possible with a little humor expression."}]
 
-openai.api_key = st.secrets["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"]
+def generate_response(prompt):
+
+    st.session_state['prompts'].append({"role": "user", "content":prompt})
+    completion=openai.ChatCompletion.create(
+        model="gpt-4",
+        messages = st.session_state['prompts']
+    )
+    
+    message=completion.choices[0].message.content
+    return message
+
+sound = BytesIO()
+
+placeholder = st.container()
+
+placeholder.title("Yeyu's Voice ChatBot")
+stt_button = Button(label='SPEAK', button_type='success', margin = (5, 5, 5, 5), width=200)
 
 
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var value = "";
+    var rand = 0;
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = false;
+    recognition.interimResults = true;
+    recognition.lang = 'en';
 
+    document.dispatchEvent(new CustomEvent("GET_ONREC", {detail: 'start'}));
+    
+    recognition.onspeechstart = function () {
+        document.dispatchEvent(new CustomEvent("GET_ONREC", {detail: 'running'}));
+    }
+    recognition.onsoundend = function () {
+        document.dispatchEvent(new CustomEvent("GET_ONREC", {detail: 'stop'}));
+    }
+    recognition.onresult = function (e) {
+        var value2 = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+                rand = Math.random();
+                
+            } else {
+                value2 += e.results[i][0].transcript;
+            }
+        }
+        document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: {t:value, s:rand}}));
+        document.dispatchEvent(new CustomEvent("GET_INTRM", {detail: value2}));
 
+    }
+    recognition.onerror = function(e) {
+        document.dispatchEvent(new CustomEvent("GET_ONREC", {detail: 'stop'}));
+    }
+    recognition.start();
+    """))
 
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-4"
+result = streamlit_bokeh_events(
+    bokeh_plot = stt_button,
+    events="GET_TEXT,GET_ONREC,GET_INTRM",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+tr = st.empty()
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+if 'input' not in st.session_state:
+    st.session_state['input'] = dict(text='', session=0)
 
-if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+tr.text_area("**Your input**", value=st.session_state['input']['text'])
 
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        for response in openai.ChatCompletion.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        ):
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
-  
+if result:
+    if "GET_TEXT" in result:
+        if result.get("GET_TEXT")["t"] != '' and result.get("GET_TEXT")["s"] != st.session_state['input']['session'] :
+            st.session_state['input']['text'] = result.get("GET_TEXT")["t"]
+            tr.text_area("**Your input**", value=st.session_state['input']['text'])
+            st.session_state['input']['session'] = result.get("GET_TEXT")["s"]
+
+    if "GET_INTRM" in result:
+        if result.get("GET_INTRM") != '':
+            tr.text_area("**Your input**", value=st.session_state['input']['text']+' '+result.get("GET_INTRM"))
+
+    if "GET_ONREC" in result:
+        if result.get("GET_ONREC") == 'start':
+            placeholder.image("recon.gif")
+            st.session_state['input']['text'] = ''
+        elif result.get("GET_ONREC") == 'running':
+            placeholder.image("recon.gif")
+        elif result.get("GET_ONREC") == 'stop':
+            placeholder.image("recon.jpg")
+            if st.session_state['input']['text'] != '':
+                input = st.session_state['input']['text']
+                output = generate_response(input)
+                st.write("**ChatBot:**")
+                st.write(output)
+                st.session_state['input']['text'] = ''
+
+                tts = gTTS(output, lang='en', tld='com')
+                tts.write_to_fp(sound)
+                st.audio(sound)
+
+                st.session_state['prompts'].append({"role": "user", "content":input})
+                st.session_state['prompts'].append({"role": "assistant", "content":output})
